0f1a0d8fc384c89d8072fc1dabb1103a509f90bc
chenmiaomiao_a <chenmiaomiao199305@163.com>
2020-04-30 13:38:25 +0800

    change settings中读取.env的位置

--- HelloDjango/settings.py
+++ HelloDjango/settings.py
@@ -19,8 +19,7 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-# reading .env file
-environ.Env.read_env()
+
 
 
 # False if not in os.environ
@@ -30,6 +29,9 @@ DEBUG = env('DEBUG')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# reading .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
